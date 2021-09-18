51e83fd317fd480417102b3046915e3a5ee20ac6
Hendrix1995 <83294139+Hendrix1995@users.noreply.github.com>
2021-09-19 01:37:07 +0900

    배경, Header, MakeToDo CSS 틀 수정 (#9)
    * MakeTodo 컴포넌트 input 창 구현
    
    * 배경, Header, MakeToDo CSS 틀 수정
    
    Co-authored-by: jamiep9rk <jamiecareer96@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
     //* 로고, 계정정보, 로그아웃
       <div className="header--wrapper">
         <span className="header--todo__logo">
-          <i class="fas fa-clipboard-list"></i>
+        <i class="fas fa-user-circle"></i>
         </span>
-          <span className="span--Username">유저 이름: kimcoding </span>
-          <button className="btn--logout">로그아웃</button>
+          <span className="span--Username">Admin</span>
+          <button className="btn--logout">Logout</button>
         </div>
     )
 }
