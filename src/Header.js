cb6259970f35a1d86de14ce48f3670a59cf53c22
anniemon <seojung19@gmail.com>
2021-09-18 19:32:59 +0900

    헤더 틀 만들기

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
-    return (
-        <div />
+
+  return (
+    //* 로고, 계정정보, 로그아웃
+      <div className="header--wrapper">
+        <span className="header--todo__logo">
+          <i class="fas fa-clipboard-list"></i>
+        </span>
+          <span className="span--Username">유저 이름: kimcoding </span>
+          <button className="btn--logout">로그아웃</button>
+        </div>
     )
 }
 
