0068c4c41e4c5be21bb33b013ebe10808f8d49ad
Lawen-s <bg920611@gmail.com>
2021-09-19 21:44:29 +0900

    더미데이터 삭제기능 구현

--- src/component/ContentTodo.js
+++ src/component/ContentTodo.js
@@ -9,48 +9,40 @@ const ContentTodo = () => {
 
     //setDatas - todo 제거, 생성
 
+    const deleteTodo = (some, deleteId) => {
+        const filterTodo = datas.filter((todo, id) => id !== deleteId - 1)
+        setDatas(filterTodo);
+
+    };
 
     return (
         <div>
-            <ul className="todoLists">
-                <li className="todoList">
-                    {/* 각 리스트 항목들 */}
-                    <div className="todoDate">{/*D-day표시*/}</div>
-                    <div className="todoContent">{/*todo내용표시*/}
-                        todo 내용
-                        <span className="todoContent_checkbox">
-                            {/*체크박스영역*/}
-                            <input type="checkbox">{/*체크박스*/}</input>
-                        </span>
-                        <span className="todoContent_delete">
-                            {/*삭제영역*/}
-                            <button className="todoContent_deleteButton">{/*삭제버튼*/}<i className="far fa-trash-alt"></i></button>
-                        </span>
-                    </div>
-                    {datas.map((todo) => {
-                        return (
-                            <li className="todoList">
-                                {/* 각 리스트 항목들 */}
-                                <div className="todoDate">{/*D-day표시*/}</div>
-                                <div className="todoContent">{/*todo내용표시*/}
 
-                                    {todo.content}
+            {datas.map((todo) => {
+                return (
+                    <li className="todoList">
+                        {/* 각 리스트 항목들 */}
+                        <div className="todoDate">{/*D-day표시*/}</div>
+                        <div className="todoContent">{/*todo내용표시*/}
 
-                                    <span className="todoContent_checkbox">
-                                        {/*체크박스영역*/}
-                                        <input type="checkbox">{/*체크박스*/}</input>
-                                    </span>
-                                    <span className="todoContent_delete">
-                                        {/*삭제영역*/}
-                                        <button className="todoContent_deleteButton">{/*삭제버튼*/}<i className="far fa-trash-alt"></i></button>
-                                    </span>
-                                </div>
-                            </li>
-                        )
-                    })}
-                </li>
-            </ul>
-        </div>
+                            {todo.content}
+
+                            <span className="todoContent_checkbox">
+                                {/*체크박스영역*/}
+                                <input type="checkbox">{/*체크박스*/}</input>
+                            </span>
+                            <span className="todoContent_delete">
+                                {/*삭제영역*/}
+                                <button
+                                    className="todoContent_deleteButton"
+                                    onClick={() => deleteTodo(todo.content, todo.id)}
+                                >{/*삭제버튼*/}<i className="far fa-trash-alt"></i></button>
+                            </span>
+                        </div>
+                    </li>
+                )
+            })}</div>
+
     )
 }
 
