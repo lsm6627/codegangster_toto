fa27c7fda00d6c9d488b3f813eb8f0e0ef285046
anniemon <seojung19@gmail.com>
2021-09-20 19:52:14 +0900

    user_todo 필드 추가, User:todos 1:N 관계 설정

--- server/models/index.js
+++ server/models/index.js
@@ -34,4 +34,8 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+const { todos, User } = sequelize.models;
+todos.belongsTo(User);
+User.hasMany(todos);
+
 module.exports = db;
