a66d6b21fe9df8d7e4f8e0926dd352e6ac665d6c
JinHeeZeeniiLee <zniilee@gmail.com>
2021-09-20 18:18:59 +0900

    commit complete

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,24 +1,17 @@
 const models = require('../models');
-const users = require('users')
+const Users = models.Users
 
 
 module.exports ={
-    get:(req,res) => {
-        const userId = req.params.userId;
-        if(!userId){
-            return res.status(401).send('Unauthorize User.');
-        } else {
-            model.get(Number(userId), (err, result) =>{
-                if(err) {
-                    res.status(404).send("No orders found ");
-                } else {
-                    res.status(200).json(result);
-                }
-            });
-        }
+    get:async(req, res) =>{
+        const allUrlLink = await models.url.findAll();
+        res.status(200).json(allUrlLink)
     },
 
-//     post:(res.req) => {
+    post: async(req, res) =>{
+        
+    }
 
-//     }
-};
+
+}
+    
