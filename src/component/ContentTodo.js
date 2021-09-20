a4963361b3eb48e5c2ca7f5d5f705bd97fbec609
Hendrix1995 <83294139+Hendrix1995@users.noreply.github.com>
2021-09-21 01:04:11 +0900

    ContentTodo 기능구현과 Main CSS 병합 문제 수정 (#18)
    * 현성님꺼 Pull하기 전
    
    * MakeTodo 컴포넌트 input 창 구현
    
    * contentTodo 기초
    
    * npm업데이트
    
    * 배경, Header, MakeToDo CSS 틀 수정
    
    * yarn삭제
    
    * dummydata추가
    
    * 더미데이터 삭제기능 구현
    
    * Header, filterbar CSS 완료
    
    * 삭제기능연습
    
    * Main CSS 완성
    
    * input 일정 추가 버튼 ContentTodo와 연결, 남은 할일, clearall 버튼 구현
    
    Co-authored-by: Lawen-s <bg920611@gmail.com>
    Co-authored-by: jamiep9rk <jamiecareer96@gmail.com>
    Co-authored-by: JinHeeZeeniiLee <81749794+JinHeeZeeniiLee@users.noreply.github.com>

--- src/component/ContentTodo.js
+++ src/component/ContentTodo.js
@@ -2,24 +2,32 @@ import React, { useState } from "react";
 import "./ContentTodo.css";
 import dummy from "../static/dummyData";
 
-const ContentTodo = () => {
-  const [datas, setDatas] = useState(dummy);
+const ContentTodo = ({ datas, setDatas }) => {
 
-  //setDatas - todo 제거, 생성
+    const deleteTodo = (some, deleteId) => {
+        console.log(deleteId)
+        if (!dummy) {
+            alert('todo list가 비어있습니다.')
+            return
+        }
+        const filterTodo = datas.filter((todo, id) => {
+            console.log(id)
+            return id !== deleteId
+        })
+        setDatas(filterTodo);
 
-  return (
-    <div className="todoContainer">
-      <ul className="todoLists">
-        
-          {datas.map((todo) => {
-            return (
-              <li className="todoList">
-                {/* 각 리스트 항목들 */}
-                <div className="todoDate">{/*D-day표시*/}</div>
-                <div className="todoContent">
-                  {/*todo내용표시*/}
+    };
+    return (
+        <div>
+            <ul className="todoLists">
+                {datas.map((todo) => {
+                    return (
+                        <li className="todoList" key={todo.id}>
+                            {/* 각 리스트 항목들 */}
+                            <div className="todoDate">D - {todo.d_day}</div>
+                            <div className="todoContent">
+                                {todo.content}
 
-                  {todo.content}
 
                   <span className="todoContent_checkbox">
                     {/*체크박스영역*/}
--- src/component/CountClear.js
+++ src/component/CountClear.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './CountClear.css';
 import dummyData from '../static/dummyData';
 
-const CountClear = ({ isClear, handleClear }) => {
+const CountClear = ({ isClear, handleClear, datas, setDatas}) => {
 
   return (
+
     <div className="count_Container">
       {!datas ? <span className="span--remainTodos">비어있어요!</span>
         :<span className="span--remainTodos">남은 할 일: {datas.length}</span>}
