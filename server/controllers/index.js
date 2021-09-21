9b0ecda8bd61a235676fce50b78fe742b2f2f8a0
JinHeeZeeniiLee <zniilee@gmail.com>
2021-09-21 14:06:03 +0900

    commit server

---
+++ server/controllers/index.js
@@ -0,0 +1,36 @@
+const {User} = require('../models');
+const {todos} = require('../models');
+
+
+module.exports ={
+    get:async(req, res) =>{
+        // const allUrlLink = await models.url.findAll();
+        const modelsThing = await User.findAll();
+        console.log(User)
+
+        if(!modelsThing) res.status(404).send('that is not url');
+        res.status(200).json("plz don't make error");
+    },
+
+    post: async(req, res) =>{
+        const todo = req.body.todo;
+        // Todo api 
+        const userTodo = req.body.user_todo;
+        
+        if(!todo) {
+            res.status(404).send("you've got a Wrong");
+        } 
+        else {
+            res.status(200).json('Welcome!');
+        }
+        // const userTodo = req.body.user_todo;
+
+        // todo를 테이블에 넣어준다.
+        
+        //id는 로그인 단계에서 받아야 한다. 
+
+
+        // res 하기 전에 todo 테이블에서user id로 조회를 한다. 
+        // 그 id의 todo를 전부 모아서 보내준다. 
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require('express');
+// const fs = require("fs");
+const cors = require('cors');
+const controller = require('./controllers');
+
+const app = express();
+const port = 4000;
+
+// app.use(
+//   morgan(':method :url :status :res[content-length] - :response-time ms')
+// );
+
+app.use(cors({origin: ["http://localhost:3000"], 
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS"]}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', controller.get);
+
+
+
+let server = app.listen(port, () => {
+  console.log(`server running on ${port}`);
+})
+
+
+module.exports = server;
