b3d211358d22b1c8d663dfb250bc3b4add7344f5
anniemon <seojung19@gmail.com>
2021-09-21 21:59:56 +0900

    post 라우팅 추가, get, post 메소드 최초 구현, 로그인 더미데이터(seeder) 추가 FK 에러..migration 디버깅 필요

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,20 @@
+const { User } = require('../models');
+const { todos } = require('../models');
+
 module.exports = {
     get: async(req, res) => {
-        res.status(200).json('hello world')
+        const result = await todos.findAll().catch((err) => res.json(err));
+        if(!result) return res.status(404).json('not found')
+        res.status(200).json(result);
+    },
+    post: async(req, res) => {
+        const todo = req.body.todo;
+        if(!todo) {
+          return res.sendStatus(400);
+        }
+        const userId = await User.findOnd({where: {userId: 'kimcoding'}}).catch((err) => res.json(err));
+        console.log(userId);
+        const result = await todos.create({todo: req.body.todo}).catch((err) => res.json(err));
+        res.status(200).json(result);
     }
 }
