067b17332cd6a0ebe37eb5c14857316ac2a8c1a3
Leeseongmin <lsm6627@gmail.com>
2021-09-22 23:10:34 +0900

    체크박스기능구현

--- src/component/ContentTodo.js
+++ src/component/ContentTodo.js
@@ -3,11 +3,24 @@ import "./ContentTodo.css";
 import dummy from "../static/dummyData";
 
 const ContentTodo = ({ datas, setDatas }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   const deleteTodo = (some, deleteId) => {
     const filterTodo = datas.filter((todo) => {
       return todo.id !== deleteId;
     });
     setDatas(filterTodo);
+  };
+
+  const checkHandler = ({ target }) => {
+    setIsChecked(!isChecked);
+    if (target.checked) {
+      target.parentNode.parentNode.parentNode.parentNode.parentNode.children[1].className =
+        "checked";
+    } else {
+      target.parentNode.parentNode.parentNode.parentNode.parentNode.children[1].className =
+        "unChecked";
+    }
   };
   return (
     <div className="todo_Container">
@@ -18,10 +31,19 @@ const ContentTodo = ({ datas, setDatas }) => {
               {/* 각 리스트 항목들 */}
               <div className="todoMenu">
                 <div className="todoDate">D - {todo.d_day}</div>
+                <div className="createTodo">
+                  {new Date(todo.createdAt).toLocaleDateString("ko-kr")}
+                </div>
                 <div className="buttons">
                   <span className="todoContent_checkbox">
                     <div className="checkbox_group">
-                      <input type="checkbox" id="check" />
+                      <input
+                        type="checkbox"
+                        id="check"
+                        onChange={(e) => {
+                          checkHandler(e);
+                        }}
+                      />
                     </div>
                   </span>
                   <span className="todoContent_delete">
@@ -35,7 +57,7 @@ const ContentTodo = ({ datas, setDatas }) => {
                 </div>
               </div>
 
-              <div className="todoContent">{todo.content}</div>
+              <div>{todo.content}</div>
             </li>
           );
         })}
