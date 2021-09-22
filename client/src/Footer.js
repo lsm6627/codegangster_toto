940d3cfe3cf8c5871c5065ed6864405e56b394c9
Hendrix1995 <hgud55@naver.com>
2021-09-23 01:18:11 +0900

    fix: Footer className .Ver_1.0

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 import './Footer.css'
 
 const Footer = () => {
-  return <div>Copyright @ 2021 Code Gangsters</div>;
+  return <div className="copyright">Copyright @ 2021 Code Gangsters</div>;
 };
 export default Footer;
